8c97decca1670ab3f74e92736989b8843c8caf9c
lukedong <lukedong@tencent.com>
2021-07-25 11:34:03 +0800

    binary tree preorder traversal

--- algos/binary_tree/preorder_traversal.h
+++ algos/binary_tree/preorder_traversal.h
@@ -1,10 +1,53 @@
-//
-// Created by Luke Skywalker on 2021/7/18.
-//
-
 #ifndef EVERFLOW_ALGOS_BINARY_TREE_PREORDER_TRAVERSAL_H_
 #define EVERFLOW_ALGOS_BINARY_TREE_PREORDER_TRAVERSAL_H_
 
 // https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
 
+
+namespace {
+
+// 个人递归
+class Solution {
+ public:
+  vector<int> preorderTraversal(TreeNode *root) {
+    if (root == nullptr) {
+      return {};
+    }
+    vector<int> result;
+    result.push_back(root->val);
+    auto left = preorderTraversal(root->left);
+    result.insert(result.end(), left.begin(), left.end());
+    auto right = preorderTraversal(root->right);
+    result.insert(result.end(), right.begin(), right.end());
+    return result;
+  }
+};
+
+// 官方非递归
+class Solution {
+ public:
+  vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> res;
+    if (root == nullptr) {
+      return res;
+    }
+
+    stack<TreeNode*> stk;
+    TreeNode* node = root;
+    while (!stk.empty() || node != nullptr) {
+      while (node != nullptr) {
+        res.emplace_back(node->val);
+        stk.emplace(node);
+        node = node->left;
+      }
+      node = stk.top();
+      stk.pop();
+      node = node->right;
+    }
+    return res;
+  }
+};
+
+
+} // namespace
 #endif //EVERFLOW_ALGOS_BINARY_TREE_PREORDER_TRAVERSAL_H_
