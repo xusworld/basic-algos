21741d90d7d1ca309a04cef03297bcc311907f2f
lukedong <lukedong@tencent.com>
2021-07-25 11:27:50 +0800

    binary tree inorder traversal

--- algos/binary_tree/inorder_traversal.h
+++ algos/binary_tree/inorder_traversal.h
@@ -2,5 +2,80 @@
 #define EVERFLOW_ALGOS_BINARY_TREE_INORDER_TRAVERSAL_H_
 
 // https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+// 94. 二叉树的中序遍历
+// 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+
+namespace {
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// 个人实现的递归解法
+class Solution {
+ public:
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> result;
+    if (root == nullptr) {
+      return result;
+    }
+    auto left = inorderTraversal(root->left);
+    result.insert(result.end(), left.begin(), left.end());
+    result.push_back(root->val);
+    auto right = inorderTraversal(root->right);
+    result.insert(result.end(), right.begin(), right.end());
+    return result;
+  }
+};
+
+// 官方递归解法
+class Solution {
+ public:
+  void inorder(TreeNode *root, vector<int> &res) {
+    if (!root) {
+      return;
+    }
+    inorder(root->left, res);
+    res.push_back(root->val);
+    inorder(root->right, res);
+  }
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> res;
+    inorder(root, res);
+    return res;
+  }
+};
+
+// 官方迭代解法
+class Solution {
+ public:
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> res;
+    stack <TreeNode *> stk;
+    while (root != nullptr || !stk.empty()) {
+      while (root != nullptr) {
+        stk.push(root);
+        root = root->left;
+      }
+      root = stk.top();
+      stk.pop();
+      res.push_back(root->val);
+      root = root->right;
+    }
+    return res;
+  }
+};
+
+} // namespace
+
 
 #endif //EVERFLOW_ALGOS_BINARY_TREE_INORDER_TRAVERSAL_H_
--- algos/leetcode/todo_946_validate_stack_sequeues.h
+++ algos/leetcode/todo_946_validate_stack_sequeues.h
@@ -1,11 +1,8 @@
-#ifndef EVERFLOW_ALGOS_LEETCODE_946_VALIDATE_STACK_SEQUEUES_H_
-#define EVERFLOW_ALGOS_LEETCODE_946_VALIDATE_STACK_SEQUEUES_H_
 
-namespace algos {
+固定映射区（Fixing Mapping Region）：该区域和4G的顶端只有4k的隔离带，其每个地址项都服务于特定的用途，如ACPI_BASE等。
 
+永久内存映射区（PKMap Region）：该区域可访问高端内存。访问方法是使用alloc_page(_GFP_HIGHMEM)分配高端内存页或者使用kmap函数将分配到的高端内存映射到该区域。
 
+动态内存映射区（Vmalloc Region）：该区域由内核函数vmalloc来分配，特点是：线性空间连续，但是对应的物理空间不一定连续。vmalloc分配的线性地址所对应的物理页可能处于低端内存，也可能处于高端内存。
 
-
-} // namespace algos
-
-#endif //EVERFLOW_ALGOS_LEETCODE_946_VALIDATE_STACK_SEQUEUES_H_
+直接映射区（Direct Memory Region）：线性空间中从3G开始最大896M的区间，为直接内存映射区，该区域的线性地址和物理地址存在线性转换关系：线性地址=3G+物理地址。
